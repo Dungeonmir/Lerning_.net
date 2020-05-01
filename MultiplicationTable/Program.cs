b1128f1a11bec2e28e48861c478b3c5d7c99faf6
Dungeonmir <63905738+Dungeonmir@users.noreply.github.com>
2020-05-01 13:26:27 +0300

    MultiplicationTable

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[10, 10];
+            int rows = 10;
+            int columns = 10;
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 1; j < columns; j++)
+                {
+                    array[i,j] = i * j;
+                    Console.Write($"{array[i,j]} \t");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
